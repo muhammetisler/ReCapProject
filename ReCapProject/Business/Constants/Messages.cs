97ba80a2828cbd769ff6b81b8136b95fef91ac90
muhammetisler <55495162+muhammetisler@users.noreply.github.com>
2021-03-01 21:28:15 +0300

    Add files via upload

--- ReCapProject/Business/Constants/Messages.cs
+++ ReCapProject/Business/Constants/Messages.cs
@@ -1,30 +1,37 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
 {
     public static class Messages
     {
-        public static string CarAdded = "Araç başarıyla eklendi";
-        public static string CarDeleted = "Araç silindi";
-        public static string CarUpdated = "Araç güncellendi";
-        public static string NameInvalid = "İsim geçersiz";
-        public static string PriceInvalid = "Fiyat geçersiz";
-        public static string MaintenanceTime = "Sistem bakımda";
-        public static string ProductsListed = "Ürünler listelendi";
-        public static string BrandAdded = "Marka başarıyla eklendi";
-        public static string BrandDeleted = "Marka silindi";
-        public static string BrandUpdated = "Marka güncellendi";
-        public static string ColorAdded = "Renk başarıyla eklendi";
-        public static string ColorDeleted = "Renk silindi";
-        public static string ColorUpdated = "Renk güncellendi";
-        public static string CustomerAdded = "Müşteri başarıyla eklendi";
-        public static string CustomerDeleted = "Müşteri silindi";
-        public static string CustomerUpdated = "Müşteri güncellendi";
-        public static string RentalAdded = "Kiralama başarılı";
-        public static string UserAdded = "Kullanıcı eklendi";
 
+        public static string Added = "Add process OK";
+        public static string NotAdded = "Add process NOT OK";
 
+        public static string Deleted = "Delete process OK";
+        public static string NotDeleted = "Delete process NOT OK";
+
+        public static string Updated = "Update process OK";
+        public static string NotUpdated = "Update process NOT OK";
+
+        public static string Listed = "List process OK";
+        public static string NotListed = "List process NOT OK";
+
+        public static string MaintenanceTime = "System is under maintenance";
+        public static string FailedRental = "The car has not yet been delivered";
+        public static string CarImageLimitExceeded = "More than 5 images cannot be added";
+        public static string NoCarImages = "The car does NOT have any images";
+        
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "PasswordError";
+        public static string SuccessfulLogin = "SuccessfulLogin";
+        public static string UserAlreadyExists = "UserAlreadyExists";
+        public static string UserRegistered = "SuccessUserRegistered";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+        public static string AuthorizationDenied = "AuthorizationDenied";
     }
 }
